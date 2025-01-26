ab1f98963ad174373844e0b6fbe7ea59645d493c
LCRONG <623732711@qq.com>
2025-01-26 12:39:49 +0800

    解析xml文件 默认构建mod_mariadb

--- src/mod/lcr/mod_lcr_xml/mod_lcr_xml.c
+++ src/mod/lcr/mod_lcr_xml/mod_lcr_xml.c
@@ -11,6 +11,11 @@ struct xml_binding_ud_s
 };
 typedef struct xml_binding_ud_s xml_binding_ud_t;
 
+typedef struct tmp_file {
+	char name[512];
+	int fd;
+} tmp_file_s;
+
 static struct {
 	switch_memory_pool_t *pool;
 	char *odbc_dsn;
@@ -49,6 +54,51 @@ static switch_xml_t get_directory_xml(const char *section, const char *tag_name,
 	switch_cache_db_execute_sql2str(dbh, ud_obj->query_sql, globals.user_xml_str,globals.user_xml_str_size,NULL);
 	switch_cache_db_release_db_handle(&dbh);
 	if (!(user_xml = switch_xml_parse_str_dup(globals.user_xml_str))) {
+		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "xml parse fail!\n");
+	}
+
+	return user_xml;
+}
+
+static switch_xml_t lx_xml_sql_fetch(const char *section, const char *tag_name, const char *key_name, const char *key_value, switch_event_t *params,
+								  void *user_data)
+{
+	switch_uuid_t uuid;
+	char uuid_str[SWITCH_UUID_FORMATTED_LENGTH + 1];
+	tmp_file_s tmp_file_obj;
+	xml_binding_ud_t *ud_obj = (xml_binding_ud_t *) user_data;
+	switch_xml_t user_xml = NULL;
+	switch_cache_db_handle_t *dbh = NULL;
+	ssize_t user_xml_str_len;
+	ssize_t write_res;
+	/* Initialize database */
+	if (!(dbh = lx_get_db_handle())) {
+		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Cannot open DB!\n");
+		return NULL;
+	}
+
+	switch_cache_db_execute_sql2str(dbh, ud_obj->query_sql, globals.user_xml_str,globals.user_xml_str_size,NULL);
+	switch_cache_db_release_db_handle(&dbh);
+
+	/* 创建存储xml的临时文件 */
+	memset(&tmp_file_obj, 0, sizeof(tmp_file_obj));
+	switch_uuid_get(&uuid);
+	switch_uuid_format(uuid_str, &uuid);
+	switch_snprintf(tmp_file_obj.name, sizeof(tmp_file_obj.name), "%s%s%s%s.tmp.xml", SWITCH_GLOBAL_dirs.temp_dir, SWITCH_PATH_SEPARATOR, "lcr_xml_",uuid_str);
+	if ((tmp_file_obj.fd = open(tmp_file_obj.name, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR)) <= -1) {
+		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Open the %s file fail!\n",tmp_file_obj.name);
+	}
+	/* 计算字符串的长度 */
+	user_xml_str_len = strlen(globals.user_xml_str);
+	/* 写入数据到文件 */
+	write_res = write(tmp_file_obj.fd, globals.user_xml_str, user_xml_str_len);
+
+	if (write_res != user_xml_str_len) {
+		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Failed to write to the %s file!\n",tmp_file_obj.name);
+	}
+
+	close(tmp_file_obj.fd);
+	if (!(user_xml = switch_xml_parse_file(tmp_file_obj.name))) {
 		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "xml parse fail!\n");
 	}
 
@@ -103,7 +153,7 @@ static switch_status_t load_config(void)
 		binding_ud_obj->query_sql = switch_core_strdup(globals.pool, query_sql);
 		binding_ud_obj->bind_mask = switch_core_strdup(globals.pool, bind_mask);
 
-		switch_xml_bind_search_function(get_directory_xml, switch_xml_parse_section_string(binding_ud_obj->bind_mask),binding_ud_obj);
+		switch_xml_bind_search_function(lx_xml_sql_fetch, switch_xml_parse_section_string(binding_ud_obj->bind_mask),binding_ud_obj);
 		binding_ud_obj = NULL;
 	}
 
@@ -138,6 +188,7 @@ SWITCH_MODULE_LOAD_FUNCTION(mod_lcr_xml_load)
 SWITCH_MODULE_SHUTDOWN_FUNCTION(mod_lcr_xml_shutdown)
 {
 	switch_xml_unbind_search_function_ptr(get_directory_xml);
+	switch_xml_unbind_search_function_ptr(lx_xml_sql_fetch);
 	switch_safe_free(globals.odbc_dsn);
 	return SWITCH_STATUS_SUCCESS;
 }
